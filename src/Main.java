3e6890a05ea02bb82eced3ecf50f4bfc47e96ada
Shehab Alnaimi <shehab.alnaimi@sjsu.edu>
2021-09-11 09:46:08 -0700

    Added javadoc comments to Student.java and Main.java classes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
-
+/**
+ * Main class serving as a runner for the project
+ * 
+ * @author shehab
+ *
+ */
 public class Main {
-
+	/**
+	 * Main method, where a student object is created with a name passed to the
+	 * constructor. Then the value of the instance variable is printed with a
+	 * toString() method
+	 * 
+	 * @param args - String []
+	 */
 	public static void main(String[] args) {
 
 		Student student = new Student("Tom");
--- src/Student.java
+++ src/Student.java
@@ -1,22 +1,32 @@
+/**
+ * This class represents a student object with one instance variable
+ * 
+ * @author shehab
+ *
+ */
 public class Student {
-    private String name;
+	private String name;
 
-    public Student(String name) {
-        this.name = name;
-    }
+	/**
+	 * Class constructor accepting String argument
+	 * 
+	 * @param name - String representing student's name
+	 */
+	public Student(String name) {
+		this.name = name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
-    @Override
-    public String toString() {
-        return "Student{" +
-                "name='" + name + '\'' +
-                '}';
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Student{" + "name='" + name + '\'' + '}';
+	}
 
 }
